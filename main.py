696a5aaf232817946474bf6732e3098b5dbd8713
Crystallek <99501451+Crystallek@users.noreply.github.com>
2022-08-18 11:18:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ tokenTrivia = "824c28fc3cmshfa0dfba9bd5ff5bp1bdfbfjsnb90fc16d0447"
 groupDmName = "groupname"
 
 friendsToSpam = [] #max 9 friends for now (paste friends ids as strings here pls, one friend, one string in a list)
+#btw insert only the ids of your friends, you cant insert a random person here
 
 headersDiscordData = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36',
