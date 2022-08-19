6f1eb1be6399a444da03f8b9bb62908541a6e2f5
Crystallek <99501451+Crystallek@users.noreply.github.com>
2022-08-19 15:58:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests, json, time, deep_translator
 
-tokenDiscord = "" # insert ur token here (bot tokens wont work)
+with open("data/data.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    tokenDiscord = lines[2].removeprefix("token=").replace("\n", "")
+    groupDmName = lines[3].removeprefix("groupname=").replace("\n", "")
+    targetLang = lines[4].removeprefix("targetlang=").replace("\n", "") # insert ur token here (bot tokens wont work)
+    friendsToSpam = lines[5].removeprefix("friendstospam=").replace("\n", "").replace(" ", "").replace("\"", "").split(",")
+    f.close()
+
+print(targetLang, type(targetLang))
+print(groupDmName)
 tokenTrivia = "824c28fc3cmshfa0dfba9bd5ff5bp1bdfbfjsnb90fc16d0447"
-groupDmName = "groupname"
-targetLang = None # example: "de", translation slows down the spam btw
-
-friendsToSpam = [] #max 9 friends for now (paste friends ids as strings here pls, one friend, one string in a list)
-#btw insert only the ids of your friends, you cant insert a random person here
-#example: friendsToSpam = ["123", "456"]
-
 headersDiscordData = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36',
     'Authorization': tokenDiscord,
@@ -27,8 +29,10 @@ def createGroupDM():
         exit()
     else:
         jsonDiscordData = {"recipients": friendsToSpam}
+        print(friendsToSpam)
         rDis = requests.post(f"https://discord.com/api/v9/users/@me/channels", headers=headersDiscordData, json=jsonDiscordData)
         jDis = json.loads(rDis.text)
+        print(jDis)
         return jDis["id"]
 
 def changeGroupDMName(channel):
@@ -42,14 +46,17 @@ def spam(channel):
         rTrivia = requests.get("https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia", headers=headersFactsData)
         jTrivia = json.loads(rTrivia.text)
 
-        if targetLang != None:
+        if targetLang.lower() != "none":
             try:
-                question = deep_translator.GoogleTranslator(source='auto', target=targetLang.lower()).translate(jTrivia[0]['question'])
-                answer = deep_translator.GoogleTranslator(source='auto', target=targetLang.lower()).translate(jTrivia[0]['answer'])
+                question = deep_translator.GoogleTranslator(source='auto', target="de").translate(jTrivia[0]['question'])
+                answer = deep_translator.GoogleTranslator(source='auto', target="de").translate(jTrivia[0]['answer'])
             except Exception as e:
                 print(f"Could not translate. {e}")
-        question = jTrivia[0]['question']
-        answer = jTrivia[0]['answer']
+                question = jTrivia[0]['question']
+                answer = jTrivia[0]['answer']
+        else:
+            question = jTrivia[0]['question']
+            answer = jTrivia[0]['answer']
         jsonDiscordData = {"content": f"{question}? {answer}! @everyone"}
 
         rDis = requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headersDiscordData, json=jsonDiscordData)
