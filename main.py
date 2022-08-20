73107dc97c9e031fc2f21c4eb8f8ffce84b9599f
Crystallek <99501451+Crystallek@users.noreply.github.com>
2022-08-20 08:20:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ print(f"""{colorama.Fore.BLUE}  _____ _____  _____  _____ ____  _____  _____    
 try:
     with open("data/data.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
-        tokenDiscord = lines[2].removeprefix("token=").replace("\n", "")
-        groupDmName = lines[3].removeprefix("groupname=").replace("\n", "")
-        targetLang = lines[4].removeprefix("targetlang=").replace("\n", "") 
-        friendsToSpam = lines[5].removeprefix("friendstospam=").replace("\n", "").replace(" ", "").replace("\"", "").split(",")
+        tokenDiscord = lines[0].removeprefix("token=").replace("\n", "")
+        groupDmName = lines[1].removeprefix("groupname=").replace("\n", "")
+        targetLang = lines[2].removeprefix("targetlang=").replace("\n", "") 
+        friendsToSpam = lines[3].removeprefix("friendstospam=").replace("\n", "").replace(" ", "").replace("\"", "").split(",")
         f.close()
     print(f"{colorama.Fore.GREEN}[SUCCESS]{colorama.Fore.RESET} Settings successfully loaded.\n" + "="*50 + f"\nToken: {tokenDiscord}\nGroup Name: {groupDmName}\nLanguage: {targetLang}\nFriends to spam: {friendsToSpam}\n" + "="*50)
 except:
